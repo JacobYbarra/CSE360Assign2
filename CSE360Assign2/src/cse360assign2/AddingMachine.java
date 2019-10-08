767a04b78ff2aaad76a955f274e09c3a4a4fee23
Foxtrot <Foxtrot@LapFox>
2019-10-07 17:24:30 -0700

    Initial AddingMachine setup.

---
+++ CSE360Assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assign2;
+/**
+ * An adding machine, adds and subtracts values while keeping history.
+ * @author Foxtrot
+ *<p>
+ *ClassID: 1213015295
+ */
+public class AddingMachine {
+
+	private int total;
+	/**
+	 * The Constructor method, creates the object.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * 
+	 * @return an integer that is the total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * Adds the int to the int on record.
+	 * @param value the int that is being added
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * Subtracts the int from the int on record.
+	 * @param value the int that is being subtracted from the int on record.
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * 	Converts the values to a String object.
+	 */
+	public String toString () {
+		return "";
+	}
+	/**
+	 * Clears the input of any values
+	 */
+	public void clear() {
+	
+	}
+}
